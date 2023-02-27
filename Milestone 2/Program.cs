3f2080081b36ad43020b41891f640f1694249cee
scott-w-maxwell <scottwillardmaxwell@protonmail.com>
2023-02-26 17:53:46 -0700

    Milestone 2, CST150

--- Milestone 2/Program.cs
+++ Milestone 2/Program.cs
@@ -29,21 +29,25 @@
         return deviceType;
     }
 
+    // The field should be formatted like: {GPU Name, # of GDDR Gigabytes}
     public string[] modifyGraphicsProcessingUnit(string[] newGPU){
         graphicsProcessingUnit = newGPU;
         return graphicsProcessingUnit;
     }
 
+    // The field should be formatted like: {CPU Name, # of Cores, # of Threads, Clock Speed}
     public string[] modifyCoreProcessingUnit(string[] newCPU){
         coreProcessingUnit = newCPU;
         return coreProcessingUnit;
     }
 
+    // The field should be formatted like: {RAM Name, DDR#, # of Gb}
     public string[] modifyRandomAccessMemory(string[] newRAM){
         randomAccessMemory = newRAM;
         return coreProcessingUnit;
     }
 
+    // The field should be formatted like: {Screen size, screen resolution}
     public string[] modifyScreen(string[] newScreen){
         screen = newScreen;
         return screen;
@@ -67,7 +71,7 @@
         myItem.modifyGraphicsProcessingUnit(new string[] {"Nvidia GeForce GTX 1050", "16GB"});
         myItem.modifyCoreProcessingUnit(new string[] {"Intel Core i5", "4", "16", "3.5"});
         myItem.modifyRandomAccessMemory(new string[] {"GSkill RAM", "8GB", "DDR4"});
-        myItem.modifyScreen(new string[] {"15.6 inch FHD"});
+        myItem.modifyScreen(new string[] {"15.6 inch", "1920x1080"});
         myItem.modifyQuantity(10);
 
         // Print out the updated values
@@ -89,7 +93,7 @@
         myItem.modifyGraphicsProcessingUnit(new string[] {"Nvidia GeForce RTX 3080", "32GB"});
         myItem.modifyCoreProcessingUnit(new string[] {"AMD Ryzen 9 5950X", "16", "64", "4.9"});
         myItem.modifyRandomAccessMemory(new string[] {"Corsair RAM", "32GB", "DDR5"});
-        myItem.modifyScreen(new string[] {"27 inch 4K UHD"});
+        myItem.modifyScreen(new string[] {"27 inch", "3840×2160"});
         myItem.modifyQuantity(20);
 
         // Print out the modified values
