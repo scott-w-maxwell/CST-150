42e4c7120690df585caf934eb96c33454e26bdf6
scott-w-maxwell <scottwillardmaxwell@protonmail.com>
2023-03-04 08:25:48 -0800

    Added Activity 9

---
+++ Activity 9/Activity9/Activity9/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel;
+using System.Threading;
+using System;
+
+class Activity9
+{
+    // 1. Write a method that takes two integers and displays their sum.
+    public void getSum(int num1, int num2){}
+
+    // 2. Write a method that takes five doubles and returns their average.
+    public void getAverage(double num1, double num2, double num3, double num4, double num5){}
+     
+    // 3. Write a method that returns the sum of two randomly generated integers.
+    public void generateSum(){}
+
+    // 4.  Write a method that takes three integers and returns true if their sum is divisible by 3, false otherwise.
+    public void sumIsDivisiblyByThree(int num1, int num2, int num3){}
+
+    // 5. Write a method that takes two strings and displays the string that has fewer characters.
+    public void hasFewerChars(string string1, string string2){}
+
+    // 6. Write a method that takes an array of doubles and returns the largest value in the array.
+    public void largestValue(double[] nums){}
+
+    // 7. Write a method that generates and returns an array of fifty integer values.
+    public void generateFiftyInts(){}
+
+    // 8. Write a method that takes two bool variables and returns true if they have the same value, false otherwise.
+    public void compareBooleans(bool first, bool second){}
+
+    // 9. Write a method that takes an int and a double and returns their product.
+    public void product(double num1, double num2){}
+
+    // 10. Write a method that takes a two-dimensional array of integers and returns the average of the entries.
+    public void arrayAverage(int[][] doubleArray){}
+
+    public static void Main(string[] args)
+    {
+
+    }
+}
