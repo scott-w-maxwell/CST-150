faa41d6918595e3701c4b8bd7d3e493e644bf5f1
scott-w-maxwell <scottwillardmaxwell@protonmail.com>
2023-03-12 16:51:03 -0700

    Added unfinished Milestone3 cs files

---
+++ Milestone 3/InventoryItem.cs
@@ -0,0 +1,135 @@
+﻿/*
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * This program was written by Scott Maxwell for the Milestone Project in CST-150.
+ * This is my own original work.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+using System;
+
+namespace Inventory
+{
+    class InventoryItem
+    {
+
+        // Declare item fields
+
+        public string itemSKU;
+        public string itemName;
+        public string deviceType;
+        public string[] graphicsProcessingUnit;
+        public string[] coreProcessingUnit;
+        public string[] randomAccessMemory;
+        public string[] screen;
+        public int quantity;
+
+        // Below methods for modifying item fields
+
+        public string modifyItemSKU(string newSKU)
+        {
+            itemSKU = newSKU;
+            return itemSKU;
+        }
+
+        public string modifyItemName(string newName)
+        {
+            itemName = newName;
+            return itemName;
+        }
+
+        public string modifyDeviceType(string newType)
+        {
+            deviceType = newType;
+            return deviceType;
+        }
+
+        // The field should be formatted like: {GPU Name, # of GDDR Gigabytes}
+        public string[] modifyGraphicsProcessingUnit(string[] newGPU)
+        {
+            graphicsProcessingUnit = newGPU;
+            return graphicsProcessingUnit;
+        }
+
+        // The field should be formatted like: {CPU Name, # of Cores, # of Threads, Clock Speed}
+        public string[] modifyCoreProcessingUnit(string[] newCPU)
+        {
+            coreProcessingUnit = newCPU;
+            return coreProcessingUnit;
+        }
+
+        // The field should be formatted like: {RAM Name, DDR#, # of Gb}
+        public string[] modifyRandomAccessMemory(string[] newRAM)
+        {
+            randomAccessMemory = newRAM;
+            return coreProcessingUnit;
+        }
+
+        // The field should be formatted like: {Screen size, screen resolution}
+        public string[] modifyScreen(string[] newScreen)
+        {
+            screen = newScreen;
+            return screen;
+        }
+
+        public int modifyQuantity(int newQTY)
+        {
+            quantity = newQTY;
+            return quantity;
+        }
+
+        // Driver
+        /*
+        public static void Main(string[] args)
+        {
+
+            // Create a new Item object
+            InventoryItem myItem = new InventoryItem();
+
+            // Set the initial values using the modify methods
+            myItem.modifyItemSKU("12345");
+            myItem.modifyItemName("Asus DMZ3983");
+            myItem.modifyDeviceType("Laptop");
+            myItem.modifyGraphicsProcessingUnit(new string[] { "Nvidia GeForce GTX 1050", "16GB" });
+            myItem.modifyCoreProcessingUnit(new string[] { "Intel Core i5", "4", "16", "3.5" });
+            myItem.modifyRandomAccessMemory(new string[] { "GSkill RAM", "8GB", "DDR4" });
+            myItem.modifyScreen(new string[] { "15.6 inch", "1920x1080" });
+            myItem.modifyQuantity(10);
+
+            // Print out the updated values
+            Console.WriteLine("Item SKU: " + myItem.itemSKU);
+            Console.WriteLine("Item Name: " + myItem.itemName);
+            Console.WriteLine("Device Type: " + myItem.deviceType);
+            Console.WriteLine("Graphics Processing Unit: " + string.Join(", ", myItem.graphicsProcessingUnit));
+            Console.WriteLine("Core Processing Unit: " + string.Join(", ", myItem.coreProcessingUnit));
+            Console.WriteLine("Random Access Memory: " + string.Join(", ", myItem.randomAccessMemory));
+            Console.WriteLine("Screen: " + string.Join(", ", myItem.screen));
+            Console.WriteLine("Quantity: " + myItem.quantity);
+
+            Console.WriteLine("\n\n--- Modify the values and print them out again ---\n\n");
+
+            // Modify all the values
+            myItem.modifyItemSKU("67890");
+            myItem.modifyItemName("Dell Inspiron 15");
+            myItem.modifyDeviceType("Desktop");
+            myItem.modifyGraphicsProcessingUnit(new string[] { "Nvidia GeForce RTX 3080", "32GB" });
+            myItem.modifyCoreProcessingUnit(new string[] { "AMD Ryzen 9 5950X", "16", "64", "4.9" });
+            myItem.modifyRandomAccessMemory(new string[] { "Corsair RAM", "32GB", "DDR5" });
+            myItem.modifyScreen(new string[] { "27 inch", "3840×2160" });
+            myItem.modifyQuantity(20);
+
+            // Print out the modified values
+            Console.WriteLine("Modified values:\n");
+            Console.WriteLine("Item SKU: " + myItem.itemSKU);
+            Console.WriteLine("Item Name: " + myItem.itemName);
+            Console.WriteLine("Device Type: " + myItem.deviceType);
+            Console.WriteLine("Graphics Processing Unit: " + string.Join(", ", myItem.graphicsProcessingUnit));
+            Console.WriteLine("Core Processing Unit: " + string.Join(", ", myItem.coreProcessingUnit));
+            Console.WriteLine("Random Access Memory: " + string.Join(", ", myItem.randomAccessMemory));
+            Console.WriteLine("Screen: " + string.Join(", ", myItem.screen));
+            Console.WriteLine("Quantity: " + myItem.quantity);
+
+        }
+        */
+    }
+
+}
---
+++ Milestone 3/inventoryManager.cs
@@ -0,0 +1,152 @@
+﻿/*
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * This program was written by Scott Maxwell for the Milestone Project in CST-150.
+ * This is my own original work.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+using Inventory;
+
+class InventoryManager
+{
+
+    // Dictates size of array
+    int inventorySize;
+
+    // Tracks first open spot in array
+    int count;
+
+    // Array that holds inventoryItems
+    InventoryItem[] inventoryList;
+
+    // Constructor
+    public InventoryManager(int initialSize = 10)
+    {
+        inventorySize = initialSize;
+        inventoryList = new InventoryItem[inventorySize];
+        count = 0;
+    }
+
+    // Adds items to inventoryList
+    public void add(InventoryItem item){
+        if(count >= inventorySize)
+        {
+            // Double inventorySize
+            inventorySize *= 2;
+            Array.Resize(ref inventoryList, inventorySize);
+        }
+        inventoryList[count] = item;
+        count++;
+    }
+
+    // Removes items from inventoryList
+    public void remove(int itemIndex){
+
+        // Shift all values in the array right of the removed value left
+        for(int index = itemIndex; index < inventoryList.Length; index++)
+        {
+            inventoryList[index] = inventoryList[index + 1];
+        }
+
+        // Decrease the count of items in array
+        count--;
+
+        // Resize Array to be smaller if only using 1/4 of it.
+        if((inventorySize / count) <= (1.0/4.0)){
+            inventorySize /= 2;
+            Array.Resize(ref inventoryList, inventorySize);
+        }
+    }
+
+    // Adds QTY to a paticular item in inventoryList
+    public void restock(int itemIndex, int qtyIncrease)
+    {
+        int currentQTY = inventoryList[itemIndex].quantity;
+        inventoryList[itemIndex].modifyQuantity(currentQTY + qtyIncrease);
+    }
+
+    // lists items in inventoryList
+    public void display(){
+        for(int index = 0; index < count; index++)
+        {
+            Console.WriteLine(inventoryList[index]);
+        }
+    }
+
+    // Returns a list of items that match provided params
+    public List<InventoryItem> search(string? name = null, int? qty = null, string? deviceType = null){
+
+
+        // List to store matches
+        List<InventoryItem> matchesQuery = new List<InventoryItem>();
+
+        // Loop over inventoryList to find matches
+        for (int index = 0; index < count; count++)
+        {
+
+            InventoryItem item = inventoryList[index];
+
+            // Used to record match or a non-match
+            bool matches = false;
+
+            // If parameter was specified
+            if(name != null)
+            {
+                if(item.itemName == name)
+                {
+                    matches = true;
+                }
+                else
+                {
+                    matches = false;
+                }
+
+            }
+
+            // If parameter was specified
+            if(qty != null)
+            {
+                if(item.quantity == qty)
+                {
+                    matches = true;
+                }
+                else
+                {
+                    matches = false;
+                }
+            }
+
+            // If parameter was specified
+            if (deviceType != null)
+            {
+                if(item.deviceType == deviceType)
+                {
+                    matches = true;
+                }
+                else
+                {
+                    matches = false;
+                }
+            }
+
+            if (matches)
+            {
+                matchesQuery.Add(item);
+            }
+        }
+
+        return matchesQuery;
+    }
+
+    // Driver
+    public static void Main(string[] args)
+    {
+
+        InventoryManager inventoryManager = new InventoryManager();
+
+        InventoryItem item = new InventoryItem();
+
+        inventoryManager.add(item);
+        inventoryManager.display();
+    }
+}
